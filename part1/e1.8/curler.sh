b07806bd40b0bfddd01ef45cb305c0e965938bdc
Ragnar <tsivqdnil.rangar@gmail.com>
2022-05-10 22:54:22 +0300

    part 1.5

--- part1/e1.8/curler.sh
+++ part1/e1.8/curler.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-echo "Input website:"; read website; echo "Searching.."; sleep 1; curl http://$website;
+# was
+# echo "Input website:"; read website; echo "Searching.."; sleep 1; curl http://$website;
+
+echo "Searching..";
+sleep 1;
+curl http://$1;
